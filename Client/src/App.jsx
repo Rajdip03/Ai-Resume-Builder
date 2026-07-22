528c64ac3cb5be6c9fc0c44a7cb1bd8937fbe1d5
Rajdip03 <rajdipmondal03@gmail.com>
2026-07-22 15:13:20 +0530

    fix: load user Resumes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     const token = localStorage.getItem('token')
     try {
       if (token) {
-        const { data } = await api.get('/api/user/data', { headers: { Authorization: token } })
+        const { data } = await api.get('/api/users/data', { headers: { Authorization: token } })
         if (data.user) {
           dispatch(login({ user: data.user, token: token }))
         }
