e5b1d44a6465e5ed592414cbd8efc3ac6f1c4e63
Rajdip03 <rajdipmondal03@gmail.com>
2026-07-28 17:59:46 +0530

    fix: fix gemini api issue

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import atsRouter from "./routes/atsRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Datbase connection
+// Database connection
 await connectDB()
 
 app.use(cors());
